3f1d4d14d1eb7d8e872914a795d32cdd2b10b51b
AsimHayran <168582628+AsimHayran@users.noreply.github.com>
2025-03-02 12:38:10 +0000

    Update git.js

--- git.php
+++ git.php
@@ -1,9 +1,20 @@
- <php
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
 
+app.use(bodyParser.json());
 
-$myfile = fopen("git.txt", "w");
-$txt = "lat: " . $_GET["lat"] . "\nlong: " .  $_GET["long"];
-fwrite($myfile, $txt);
-fclose($myfile);
+app.post('/location', (req, res) => {
+    const location = req.body;
+    console.log('Received location:', location);
 
-    ?>
+    // Here you can save the location data to a database or process it as needed
+    // For example, you can send an email or log the data
+
+    res.status(200).send('Location received');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
